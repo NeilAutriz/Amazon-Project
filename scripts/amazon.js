3b7121f5802ddfc4b3efc1bb6d7c9b0680b49ac1
NeilAutriz <mngautriz@gmail.com>
2024-06-15 18:50:32 +0800

    Fix: Allowed adding to cart through the product id

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,11 +46,12 @@ function renderProducts(){
             <div class="product-spacer"></div>
     
             <div class="added-to-cart">
-            <img src="images/icons/checkmark.png">
+            <img src="images/icons/checkmark.png" class="added-image">
             Added
             </div>
     
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-button" 
+            data-product-id="${product.id}">
             Add to Cart
             </button>
         </div>
@@ -58,3 +59,23 @@ function renderProducts(){
     });
     productHTML.innerHTML = renderHTML; 
 }
+
+const addCartButton = document.querySelectorAll('.js-add-button');
+addCartButton.forEach((button) => {
+    let cartProductId = button.dataset.productId;
+    button.addEventListener('click', () => {
+        let addedProduct;
+        cart.forEach((currentProduct) => {
+            if(currentProduct.id === cartProductId){
+                addedProduct = currentProduct;
+            }
+        })
+        if(addedProduct){
+            addedProduct.quantity++;
+        } else {
+            cart.push({id: cartProductId, 
+                quantity: 1});
+        }
+        console.log(cart);
+    })
+})
