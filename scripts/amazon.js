44fca8c60de81e45149e91d2932affae407f9a24
NeilAutriz <mngautriz@gmail.com>
2024-06-15 14:17:28 +0800

    Fix: Rendered all of the products in the amazon project

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,42 +1,4 @@
-let products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating : {
-            stars: 40,
-            ratingCount : 87
-        },
-        price: 1090,
-        quantity: 1
-    },
-
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating : {
-            stars: 45,
-            ratingCount : 127
-        },
-        price: 2095,
-        quantity: 1
-    },
-    
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating : {
-            stars: 45,
-            ratingCount : 56
-        },
-        price: 799,
-        quantity: 1
-    }
-]
-
 renderProducts();
-
-
-
 
 function renderProducts(){
     let productHTML = document.querySelector('.js-product-grid');
@@ -56,14 +18,14 @@ function renderProducts(){
     
             <div class="product-rating-container">
             <img class="product-rating-stars"
-                src="images/ratings/rating-${product.rating.stars}.png">
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
-                ${product.rating.ratingCount};
+                ${product.rating.count}
             </div>
             </div>
     
             <div class="product-price">
-                $${product.price};
+                $${(product.priceCents/100).toFixed(2)}
             </div>
     
             <div class="product-quantity-container">
