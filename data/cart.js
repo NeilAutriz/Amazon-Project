138656d9f4199fa3a3f76a00cd02fc0d225d5de7
NeilAutriz <mngautriz@gmail.com>
2024-06-15 21:30:31 +0800

    Rendered the orders in the checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,14 @@
-export const cart = [];
+export const cart = [{
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2, 
+    price: 1090
+    },
+    {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1, 
+    price: 2095
+    },
+];
 
 
 export function pushToCart(cartProductId, cartProductPrice){
