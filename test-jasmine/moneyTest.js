36183b6ce8d6ef6db102297c7d97686f63fc4345
NeilAutriz <mngautriz@gmail.com>
2024-06-22 15:02:53 +0800

    Fix: Added the jasmine test framework along with the test cases

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,16 @@
+import {moneyCurrency} from '../scripts/utils/money.js'
+
+describe('Test Suite: Money Currency', () => {
+    it('Converts the Centavos into Peso', () => {
+        expect(moneyCurrency(2095)).toEqual('20.95')
+    });
+
+    it('Converts 0', () => {
+        expect(moneyCurrency(0)).toEqual('0.00');
+    })
+
+    it('Rounds to the nearest hundredth', () => {
+        expect(moneyCurrency(2000.5)).toEqual('20.01');
+    })
+
+})
